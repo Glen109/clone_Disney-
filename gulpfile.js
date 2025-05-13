9da264755a4367c041371b3a07afb6c5fe93293c
Glen109 <gledson10rodial@gmail.com>
2025-05-13 18:36:00 -0300

    criando o hero, importando fonte

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,23 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'))
+const imagemin = require('gulp-imagemin');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+
+
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'))
+}
 
 function styles() {
-    return gulp.src('./src/styles/*.scss')
-        .pipe(sass({ outputStyle: 'compressed' }))
+    return gulp.src('./src/styles/main.scss')
+        .pipe(sass.sync({style: 'compressed'}).on('error', sass.logError))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('./dist/css'))
 }
 
-exports.default = styles;
+exports.default = gulp.parallel(styles, images);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }

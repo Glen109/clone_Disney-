5987edef87fd027356db8be718e7521b2c77fc26
Glen109 <gledson10rodial@gmail.com>
2025-06-02 15:23:17 -0300

    java simplificado e ajustes

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@ const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
+const unglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(unglify())
+        .pipe(gulp.dest('./dist/js'))
+}
 
 
 function images() {
@@ -17,7 +24,8 @@ function styles() {
         .pipe(gulp.dest('./dist/css'))
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, scripts);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
